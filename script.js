9fc252aef6a0aa371e81bccf8b0bdbae2b38662f
Greg Stead <gregstead@gmail.com>
2020-10-07 19:03:23 -0500

    Got gimer to function with question progression

--- script.js
+++ script.js
@@ -9,43 +9,67 @@ var answerButton4 = document.getElementById('button-4');
 var startDiv = document.getElementById('start-div');
 var questionDiv = document.getElementById('question-div');
 
+// Elements
+var timerEl = document.getElementById('timer');
+var questionText = document.getElementById('question-text');
+
 // Object containing all questions and answer choices
 var quizQuestions = {
-    'question-0' : {
+    'question-0': {
         'question': 'Commonly used data types do NOT inlude:',
-        'answers' : ['Booleans', 'Numbers','Alerts','Strings'],
-        'correct' : 'Alerts'
+        'answers': ['Booleans', 'Numbers', 'Alerts', 'Strings'],
+        'correct': 'Alerts'
     },
-    'question-1' : {
+    'question-1': {
         'question': 'A question',
-        'answers' : ['flerp','derp','lerp','gerp'],
-        'correct' : 'derp',
+        'answers': ['flerp', 'derp', 'lerp', 'gerp'],
+        'correct': 'derp',
     }
 }
 
-startBtn.addEventListener('click', function() {
+//Starts the quiz
+startBtn.addEventListener('click', function () {
     startQuiz();
 });
 
 function startQuiz() {
     // Hide start div, show question div
     startDiv.setAttribute('class', 'container hidden');
-    questionDiv.setAttribute('class','container');
+    questionDiv.setAttribute('class', 'container');
 
-    var questionText = document.getElementById('question-text');
+    // Start timer
+    var timeLeft = 30;
+    var i = 0;
 
-    // Get questions and answer choices from quizQuestions object
-    for (var i=0; i< Object.values(quizQuestions).length;) {
-        questionText.innerText = quizQuestions['question-'+i]['question'];
+    questionDiv.addEventListener('click', function(event) {
+         if (event.target.matches('button')) {
+            if (event.target.innerText == quizQuestions['question-' + i]['correct']) {
+                console.log("yes");
+                i++;
+            } else {
+                console.log("no");
+                i++;
+            };
+         };
+    });
 
-        answerButton1.innerText = quizQuestions['question-'+i]['answers'][0];
-        answerButton2.innerText = quizQuestions['question-'+i]['answers'][1];
-        answerButton3.innerText = quizQuestions['question-'+i]['answers'][2];
-        answerButton4.innerText = quizQuestions['question-'+i]['answers'][3];
+    var timeInterval = setInterval(function () {
+        timerEl.textContent = timeLeft
+        timeLeft--;
+        
+        // Render question and answer choices
+        questionText.innerText = quizQuestions['question-' + i]['question'];
 
-        answerButton1.addEventListener('click', function(i) {
-            return i++
-        })
-    };
+        answerButton1.innerText = quizQuestions['question-' + i]['answers'][0];
+        answerButton2.innerText = quizQuestions['question-' + i]['answers'][1];
+        answerButton3.innerText = quizQuestions['question-' + i]['answers'][2];
+        answerButton4.innerText = quizQuestions['question-' + i]['answers'][3]
+
+        if (timeLeft === 0) {
+            timerEl.textContent = "";
+            clearInterval(timeInterval);
+        }
+
+    }, 1000);
 
 };
